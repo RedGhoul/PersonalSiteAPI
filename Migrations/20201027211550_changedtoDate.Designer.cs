3a757c4eda125ece6838fe6a40e56e9ed7c2ccea
RedGhoul <avaneesab5@gmail.com>
2020-10-27 17:17:48 -0400

    changed date to string

--- Migrations/20201027211550_changedtoDate.Designer.cs
+++ Migrations/20201027211550_changedtoDate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PortfolioSiteAPI.Data;
 
 namespace PortfolioSiteAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201027211550_changedtoDate")]
+    partial class changedtoDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
